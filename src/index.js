0b675c24efe8404b2eb57ea9afe08b5d97ab5f3f
Khalid Izquierdo <khalid.izquierdo.a@uni.pe>
2020-06-27 17:25:48 -0500

    Bug

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+console.log(`hola`)
 const green = document.getElementById("green")
 const red = document.getElementById("red")
 const yellow = document.getElementById("yellow")
 const blue = document.getElementById("blue")
-const button_start = document.getElementById("button_start")
+const buttonstart = document.getElementById("button_start")
 const last_level = 10
 
-class game{
+class game {
     constructor() {
+        debugger
         this.init()
+        debugger
         this.startSequence()
-		setTimeout(this.next_level, 500)
+        setTimeout(this.next_level, 500)
     }
-    init(){
+    
+    init() {
         this.togglebutton_start()
         this.level = 1
         this.colors = {
@@ -20,7 +24,7 @@ class game{
             2: yellow,
             3: blue,
         }
-        this.colorsCode = {
+            this.colorsCode = {
             green: 0,
             red: 1,
             yellow: 2,
@@ -30,49 +34,50 @@ class game{
         this.nextLvl = this.nextLvl.bind(this)
         this.init = this.init.bind(this)
     }
-    togglebutton_start(){
+    togglebutton_start() {
         const status = button_start.classList.contains("hide")
-        if(status) {
+        if (status) {
             button_start.classList.remove("hide")
-        }
-        else{
+        } else {
             button_start.classList.add("hide")
         }
     }
-    startSequence(){
+    startSequence() {
+        debugger
         this.sequence = new Array(last_level)
             .fill(0)
-            .map((n) => Math.floor(Math.random()*4))
+            .map((n) => Math.floor(Math.random() * 4))
     }
-    next_level (){
+    next_level() {
         this.sub_level = 0
         this.lightSequence()
         this.addClickEvent()
     }
-    lightColor(numberColor){
+    lightColor(numberColor) {
         this.colors[numberColor].classList.add("light")
         setTimeout(() => this.turn_off_color(numberColor), 350)
     }
-    turn_off_color(numberColor){
+    turn_off_color(numberColor) {
         this.colors[numberColor].classList.remove("light")
     }
     lightSequence() {
-        for (let i = 0; i < this.level; i++){
+        for (let i = 0; i < this.level; i++) {
             setTimeout(
                 () => this.lightColor(this.sequence[i]),
-                1000*i
+                1000 * i
             )
         }
     }
-    addClickEvent(){
+    addClickEvent() {
         for (let i = 0; i < this.level; i++) {
             this.colors[i].addEventListener(
-                "click", this.chooseColor
+                "click",
+                this.chooseColor
             )
         }
     }
-    deleteClickEvent(){
-        for (let i=0; i < 4; i++) {
+    deleteClickEvent() {
+        for (let i = 0; i < 4; i++) {
             this.colors[i].removeEventListener(
                 "click",
                 this.chooseColor
@@ -94,20 +99,21 @@ class game{
                     setTimeout(this.next_level, 1000)
                 }
             }
-        } else{
+        } else {
             this.lose()
         }
     }
     win() {
         swal("Simon Says", "You win!", "Success").then(this.init)
     }
-    lose(){
+    lose() {
         swal("Simon Says", "You lose", "Error").then(() => {
             this.deleteClickEvent()
             this.init()
         })
     }
 }
+
 function start_game() {
     window.game = new game()
 }
